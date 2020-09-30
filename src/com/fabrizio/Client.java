13ef064b4c0e73570a61ca4a513fe130915cbc7b
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-09-29 20:11:50 -0400

    setfields

---
+++ src/com/fabrizio/Client.java
@@ -0,0 +1,14 @@
+package com.fabrizio;
+
+public class Client {
+    private String name;
+    private String firstName;
+    private String lastName;
+    private String middleName;
+    private String clientId;
+    private float currentBill;
+    private String prePaid;
+    private int phoneNumber;
+    private int roomNumber;
+    private String roomType;
+}
---
+++ src/com/fabrizio/Room.java
@@ -0,0 +1,11 @@
+package com.fabrizio;
+
+public class Room {
+    private int number;
+    private String type;
+    private int floor;
+    private boolean isOccupied;
+    private boolean needsCleaning;
+    private String occupant;
+    private int avgPrice;
+}
